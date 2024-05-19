6b08c260d0b471fefa029aa347ee42554314e56c
Pritam Sharma <pritam.sharma@ingredion.com>
2024-05-19 23:46:36 +0530

    adding class based component for user card

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -1,3 +1,5 @@
+import User from "./User";
+
 const AboutUs = () => {
   return (
     <>
---
+++ src/components/User.js
@@ -0,0 +1,11 @@
+const User = () =>{
+    return (
+        <div className="user-card">
+            <h1>Name : Pritam Sharma</h1>
+            <h2>Location : Kolkata</h2>
+            <h2>Contact : sharmapritam841@gmail.com</h2>
+        </div>
+    )
+}
+
+export default User;
